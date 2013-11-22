f845a5c65809bcc71ed3e3caa081f637518cfe4e
dohmatob elvis dopgima <elvis.dohmatob@inria.fr>
2013-11-22 16:26:56 +0100

    - ENH: using fmin_l_bfgs_b instead of fmin_bfgs - NEWFEATURE: plotting energy trajectories

--- rank_k_bfgs.py
+++ rank_k_bfgs.py
@@ -5,13 +5,16 @@
 """
 
 import numpy as np
-from scipy.optimize import fmin_bfgs
+from scipy.optimize import fmin_l_bfgs_b  # XXX better than bfgs
 from sklearn.linear_model import Ridge
 from scipy.sparse.linalg import svds
+import matplotlib.pyplot as plt
 
 
 def f(U, V, X, Y, alpha1, alpha2):
     """The (non-convex) energy functional"""
+    # XXX it be nice to have f return func val and grad too
+
     loss_value = ((X.dot(U).dot(V.T) - Y) ** 2).sum()
 
     penalization = alpha1 * (U ** 2).sum() + alpha2 * (V ** 2).sum()
@@ -37,7 +40,7 @@ def grad_f(U, V, X, Y, alpha1, alpha2, out=None):
 
 def get_vec_func(X, Y, alpha1, alpha2, rank, n_features):
     """Returns a function that takes a single column vector and
-    reshapes into the appropriate rank k matrices and then calculates
+    reshapes into the apriate rank k matrices and then calculates
     the functional"""
 
     def vecfunc(vecUV):
@@ -67,16 +70,19 @@ def get_grad_func(X, Y, alpha1, alpha2, rank, n_features):
     return vecgrad
 
 
-def rank_constrained_least_squares(X, Y, rank, alpha1, alpha2=None, 
-                                   U0=None, V0=None, 
+def rank_constrained_least_squares(X, Y, rank, alpha1, alpha2=None,
+                                   U0=None, V0=None,
                                    max_bfgs_iter=500,
+                                   m=10,
                                    gradient_tolerance=1e-5,
-                                   callback=None):
-    """Minimizes 
-
+                                   callback=None,
+                                   verbose=3):
+    """
+    Minimizes
     .5 * ||XUV.T - Y|| ** 2 + .5 * alpha * (||U|| ** 2 + ||V|| ** 2)
 
     """
+
     if alpha2 is None:
         alpha2 = alpha1
 
@@ -101,10 +107,13 @@ def rank_constrained_least_squares(X, Y, rank, alpha1, alpha2=None,
 
     initial_UV_vec = np.vstack([U, V]).ravel()
 
-    result = fmin_bfgs(energy_function, x0=initial_UV_vec, 
-                       fprime=energy_gradient, maxiter=max_bfgs_iter,
-                       gtol=gradient_tolerance,
-                       callback=callback)
+    result = fmin_l_bfgs_b(energy_function,
+                           x0=initial_UV_vec,
+                           fprime=energy_gradient, maxiter=max_bfgs_iter,
+                           # gtol=gradient_tolerance,
+                           m=m,
+                           callback=callback,
+                           iprint=verbose)[0]
 
     concat_matrix = result.reshape(-1, rank)
     U_res = concat_matrix[:n_features]
@@ -133,13 +142,36 @@ if __name__ == "__main__":
     #     err = check_grad(func, gradient_of_f, vecUV)
     #     print err
 
-
-    energies = []
+    maxit = 500
     for r in [5, 6, min(X.shape[1], Y.shape[1]) - 1]:
+        print "\r\nCase: r = %i" % r
+        energies = []
         func = get_vec_func(X, Y, 1., 1., r, n_features)
-        def cb(vec):
-            print func(vec)
+        # callback_env = {"it": 0}
 
-        result = rank_constrained_least_squares(X, Y, r, 1., callback=cb)
+        def cb(vec):
+            # callback_env["it"] += 1
+            energy = func(vec)
+            # print "\titer %03i/%03i: energy = %g" % (
+            #     callback_env["it"], maxit, energy)
+            energies.append(energy)
+            # print func(vec)
+
+        result = rank_constrained_least_squares(
+            X, Y, r, 1.,
+            callback=cb,
+            m=10,  # memory budget
+            max_bfgs_iter=maxit)
+        plt.plot(energies - np.mean(energies), label="r=%i" % r)
+
+    # prettify plots
+    plt.legend()
+    plt.title(("Problem: argmin .5 * ||XUV.T - Y|| ** 2 + .5 * alpha * (||U||"
+               " ** 2 + ||V|| ** 2)"))
+    plt.ylabel("f(x_k) - f(x*)")
+    plt.xlabel("time (s)")
+    plt.yscale("log")
 
     print result
+
+    plt.show()
