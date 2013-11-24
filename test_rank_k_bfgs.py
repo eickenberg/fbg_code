91d251f329ffc22be3f46841d24a2e056725f532
Michael Eickenberg <michael.eickenberg@gmail.com>
2013-11-24 21:20:32 +0100

    rank k not working too well

--- test_rank_k_bfgs.py
+++ test_rank_k_bfgs.py
@@ -18,40 +18,64 @@ U = rng.randn(n_features, rank)
 V = rng.randn(n_targets, rank)
 
 Y_train_clean = X_train.dot(U).dot(V.T)
-signal_norms = np.sqrt(
-    ((Y_train_clean - Y_train_clean.mean(0)) ** 2).sum(axis=0))
-
+signal_stdev = Y_train_clean.std(axis=0)
 noise_vector = rng.randn(*Y_train_clean.shape)
-noise_vector /= np.sum((noise_vector ** 2).sum(0))
+noise_vector /= noise_vector.std(axis=0)
 
-Y_train_noisy = Y_train_clean + signal_norms / SNR * noise_vector
+Y_train_noisy = Y_train_clean + signal_stdev / SNR * noise_vector
 
 
 
-num_test_samples = 50
-test_SNR = 1e6
+num_test_samples = 100
+test_SNR = 100.
 X_test = rng.randn(num_test_samples, n_features)
 Y_test_clean = X_test.dot(U).dot(V.T)
-test_signal_norms = np.sqrt((Y_test_clean ** 2).sum(axis=0))
+Y_test_stdev = Y_test_clean.std(0)
+test_noise = rng.randn(*Y_test_clean.shape)
+test_noise /= test_noise.std(axis=0)
 
-Y_test_noisy = Y_test_clean + signal_norms / SNR * rng.randn(
-    *Y_test_clean.shape)
+Y_test_noisy = Y_test_clean + Y_test_stdev / test_SNR * test_noise
+
+
+from ridge import _RidgeGridCV
+
+ridge = _RidgeGridCV(alpha_min=1., alpha_max=1000., n_grid_points=5,
+                     n_grid_refinements=2, cv=2)
+
+ridge_coefs = ridge.fit(X_train, Y_train_clean).coef_.T
+
+Uridge, sridge, VridgeT = np.linalg.svd(ridge_coefs, full_matrices=False)
 
 max_rank = 20
 ranks = np.arange(max_rank) + 1
 results = []
 corr_scores = []
 r2_scores = []
+ridge_corr_scores = []
+ridge_r2_scores = []
+
 for r in ranks:
     print "Rank: %d" % r
     U_, V_, something = result = \
         rank_k_bfgs.rank_constrained_least_squares(X_train, Y_train_noisy, r,
-                                                        10., 10.)
+                                                        0., 0.,
+                   # U0=rng.randn(X_train.shape[1], r),
+                   # V0=rng.randn(Y_train_noisy.shape[1], r),
+                     max_bfgs_iter=1000
+                                                   )
     results.append(result)
 
     predictions = X_test.dot(U_).dot(V_.T)
     corr_scores.append(_multi_corr_score(Y_test_noisy, predictions))
     r2_scores.append(_multi_r2_score(Y_test_noisy, predictions))
+
+    rank_k_ridge_coefs = Uridge[:, :r].dot(
+        sridge[:r][:, np.newaxis] * VridgeT[:r, :])
+
+    ridge_predictions = X_test.dot(rank_k_ridge_coefs)
+    ridge_corr_scores.append(_multi_corr_score(Y_test_noisy,
+                                               ridge_predictions))
+    ridge_r2_scores.append(_multi_r2_score(Y_test_noisy, ridge_predictions))
 
 
 import pylab as pl
@@ -64,13 +88,44 @@ pl.errorbar(ranks,
             elinewidth=3,
             linewidth=3,
             color="r")
-pl.axis([0, max_rank, 0, 1.1])
+pl.axis([0, max_rank, -.1, 1.1])
 pl.title("Correlation scores")
 
 pl.figure()
 pl.plot(np.array(r2_scores))
-pl.axis([0, max_rank, 0, 1.1])
+r2_array = np.array(r2_scores)
+pl.errorbar(ranks, 
+            r2_array.mean(axis=1),
+            yerr=r2_array.std(axis=1),
+            elinewidth=3,
+            linewidth=3,
+            color="r")
+pl.axis([0, max_rank, -.1, 1.1])
 pl.title("R2 scores")
 
+pl.figure()
+pl.plot(np.array(ridge_corr_scores))
+ridge_corr_array = np.array(ridge_corr_scores)
+pl.errorbar(ranks, 
+            ridge_corr_array.mean(axis=1),
+            yerr=ridge_corr_array.std(axis=1),
+            elinewidth=3,
+            linewidth=3,
+            color="r")
+pl.axis([0, max_rank, -.1, 1.1])
+pl.title("Ridge projected to low rank, corr scores")
+
+pl.figure()
+pl.plot(np.array(ridge_r2_scores))
+ridge_r2_array = np.array(ridge_r2_scores)
+pl.errorbar(ranks, 
+            ridge_r2_array.mean(axis=1),
+            yerr=ridge_r2_array.std(axis=1),
+            elinewidth=3,
+            linewidth=3,
+            color="r")
+pl.axis([0, max_rank, -.1, 1.1])
+pl.title("Ridge projected to low rank, r2 scores")
 
 pl.show()
+
